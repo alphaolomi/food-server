4198a9c8d6e2da83fdd9674686835660eee96577
Alpha Olomi <alphaolomi@gmail.com>
2019-08-10 13:14:51 +0300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,4 +1,5 @@
 <?php
+$DATABASE_URL = parse_url('postgres://gtpxwewmoektaq:e69d79bd41ceec35194dbdd68a938085cb1852ea713cb5aa7a549fe90b154a4e@ec2-107-22-160-185.compute-1.amazonaws.com:5432/dcv39p8iehc4ev');
 
 return [
 
@@ -61,16 +62,15 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
-            'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require',
         ],
 
         'sqlsrv' => [
