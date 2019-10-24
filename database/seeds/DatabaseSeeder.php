4d56581534cc56ddafc81eda5c81d5a2d386c948
Alpha Olomi <alphaolomi@gmail.com>
2019-10-24 12:48:52 +0300

    fix(seeder): change symbol typo

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
-        \$this->call(FoodsTableSeeder::class);
+        $this->call([
+            UsersTableSeeder::class,
+            FoodsTableSeeder::class
+        ]);
     }
 }
