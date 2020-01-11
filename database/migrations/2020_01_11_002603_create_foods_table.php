0f4dbe2518b49cb188d67bc91f5db5053d57e485
Alpha Olomi <alphaolomi@gmail.com>
2020-01-11 03:29:22 +0300

    add foods table

---
+++ database/migrations/2020_01_11_002603_create_foods_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFoodsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('foods', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('subTitle');
+            $table->text('info');
+            $table->decimal('price',8,2);
+            $table->integer('rating');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('foods');
+    }
+}
