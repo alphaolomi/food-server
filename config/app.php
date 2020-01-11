8a34a982cbab2c5978a70facf2b938148264e0f5
Alpha Olomi <alphaolomi@gmail.com>
2020-01-11 03:22:05 +0300

    add infyom

--- config/app.php
+++ config/app.php
@@ -166,8 +166,8 @@ return [
          * Package Service Providers...
          */
         Spatie\Permission\PermissionServiceProvider::class,
-
-         OwenIt\Auditing\AuditingServiceProvider::class,
+        OwenIt\Auditing\AuditingServiceProvider::class,
+        InfyOm\CoreUITemplates\CoreUITemplatesServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -227,7 +227,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Form'      => Collective\Html\FormFacade::class,
+        'Html'      => Collective\Html\HtmlFacade::class,
+        'Flash'     => Laracasts\Flash\Flash::class,
     ],
 
 ];
