b199bddbe3d695a5d6946b4f181d9b43fa03423a
Alpha Olomi <alphaolomi@gmail.com>
2020-01-11 03:05:36 +0300

    clear

--- routes/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-// api/v1
-Route::group(['prefix' => 'v1'], function () {
-    Route::resource('foods', 'FoodAPIController');
-    Route::resource('users', 'UserAPIController')->expect('destroy');
-});
