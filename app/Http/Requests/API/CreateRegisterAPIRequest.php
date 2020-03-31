2b6cab5d399a3e43cb136c592aab510c52ca4a0c
Alpha Olomi <alphaolomi@gmail.com>
2020-03-31 08:50:59 +0300

    feat: remove password confirmation on register for API

--- app/Http/Requests/API/CreateRegisterAPIRequest.php
+++ app/Http/Requests/API/CreateRegisterAPIRequest.php
@@ -27,7 +27,7 @@ class CreateRegisterAPIRequest extends APIRequest
         return [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'password' => ['required', 'string', 'min:8'],
         ];
     }
 }
