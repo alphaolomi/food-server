940de29a68e576aa831e265d1a8cd7142b5bed88
Alpha Olomi <alphaolomi@gmail.com>
2021-06-16 15:49:21 +0300

    remove index and show from api auth

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,17 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 // Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
+Route::resource('food', FoodAPIController::class)->only(['index','show']);
+
 Route::group(['middleware' => ['auth:sanctum']], function () {
 
-    Route::resource('food', App\Http\Controllers\API\FoodAPIController::class);
+    Route::resource('food', FoodAPIController::class)->except(['index','show']);
 
     Route::group(['middleware' => ['role:customer']], function () {
         //
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@ Route::redirect('/', '/login');
 Route::get('login/{provider}', [App\Http\Controllers\Auth\LoginWithController::class,'redirectToProvider'])->name('login.provider');
 Route::get('login/{provider}/callback', [App\Http\Controllers\Auth\LoginWithController::class,'handleProviderCallback'])->name('login.provider.callback');
 
+
+// Route::resource('food', App\Http\Controllers\FoodController::class)->only(['index']);
+
 Route::group(['middleware' => ['auth']], function () {
     Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
     Route::post('/change-password', [App\Http\Controllers\ProfileController::class, 'changePassword'])->name('change-password');
